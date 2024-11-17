769924aab3d3b40e3271ee75725a5b0c222bcef4
comred <comred01@yandex.ru>
2024-11-17 10:54:46 +0500

    1711\1
    Сортировка карт

--- modules/maps.js
+++ modules/maps.js
@@ -1,22 +1,25 @@
 import data from "./data.js";
-
+// сортировка массива с данными по алфавиту
+let test = data.maps.sort()
+// находим основной блок
 let mapsDiv = document.getElementById('mapsDiv');
 
 function dataMaps(){
   for(let i=0; i<data.maps.length; i++){
+    //создаем необходимые елементы
     let detailsBlock = document.createElement('details');
     let summaryBlock = document.createElement('summary');
     let imgBlock = document.createElement('img');
     let imgBlockS = document.createElement('img');
     let hBlock = document.createElement('h5');
-
+    // наполняем элементы информацией
     hBlock.textContent = data.maps[i][0];
     imgBlock.src = data.maps[i][1];
     imgBlock.className = 'detailsImg'
     imgBlockS.src = data.maps[i][1];
+    //устанавливаем элементы в основной блок
     summaryBlock.appendChild(hBlock);
     summaryBlock.appendChild(imgBlockS);
-    
     detailsBlock.appendChild(summaryBlock);
     detailsBlock.appendChild(imgBlock)
     mapsDiv.appendChild(detailsBlock);
