a21aa8836effbbfe5a5efeac21c291d0262b2206
Com-Red <98872839+Com-Red@users.noreply.github.com>
2025-03-06 11:03:18 +0500

    123

---
+++ py/test.py
@@ -0,0 +1,12 @@
+print('Hi world')
+test = 12
+print(f'первое сообщение {test}')
+#testest = int(input('Ведите значение: '))
+def testdef (n):
+    print(n+2)
+
+#testdef(testest)
+
+number1 = int(input('Введите число 1: '))
+number2 = int(input('Введите число 2: '))
+print(number1 ** number2)
